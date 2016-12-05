526923f276209051174bb5e82a9637d6077b90a6
Fredrik Ljung <ljung@live.se>
2016-12-05 22:54:04 +0100

    Solved day 3 part two

--- AdventOfCode/Day3/Program.cs
+++ AdventOfCode/Day3/Program.cs
@@ -6,24 +6,54 @@ namespace Day3
     {
         static void Main(string[] args)
         {
-            int count = 0;
+            int partOneCount = 0;
+            int partTwoCount = 0;
             string puzzleInput = File.ReadAllText("puzzleInput.txt");
-            var triangles = SplitString(puzzleInput, Environment.NewLine);
+            var rows = SplitString(puzzleInput, Environment.NewLine);
 
-            foreach (var triangle in triangles)
+            foreach (var row in rows)
             {
-                int a = int.Parse(triangle.Substring(2, 3));
-                int b = int.Parse(triangle.Substring(7, 3));
-                int c = int.Parse(triangle.Substring(12, 3));
+                int a = int.Parse(row.Substring(2, 3));
+                int b = int.Parse(row.Substring(7, 3));
+                int c = int.Parse(row.Substring(12, 3));
 
-                if (isValidTriangle(a, b, c))
-                    count++;
+                if (IsValidTriangle(a, b, c))
+                    partOneCount++;
             }
-            Console.WriteLine(count);
+
+            int rowCount = 0;
+            while (rowCount < rows.Length)
+            {
+                int[] col1 = new int[3];
+                int[] col2 = new int[3];
+                int[] col3 = new int[3];
+                for (int i = 0; i < 3; i++)
+                {
+                    string currentRow = rows[i + rowCount];
+                    col1[i] = int.Parse(currentRow.Substring(2, 3));
+                    col2[i] = int.Parse(currentRow.Substring(7, 3));
+                    col3[i] = int.Parse(currentRow.Substring(12, 3));
+                }
+
+                if (IsValidTriangle(col1))
+                    partTwoCount++;
+                if (IsValidTriangle(col2))
+                    partTwoCount++;
+                if (IsValidTriangle(col3))
+                    partTwoCount++;
+
+                rowCount += 3;
+            }
+
+            Console.WriteLine("Part 1 count: " + partOneCount);
+            Console.WriteLine("Part 2 count: " + partTwoCount);
             Console.ReadLine();
         }
-
-        private static bool isValidTriangle(int a, int b, int c)
+        private static bool IsValidTriangle(int[] array)
+        {
+            return IsValidTriangle(array[0], array[1], array[2]);
+        }
+        private static bool IsValidTriangle(int a, int b, int c)
         {
             return (a + b > c) && (a + c > b) && (b + c > a);
         }
