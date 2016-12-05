c27ca08f9441fe240b166a2c5fc253180fdb6284
Fredrik Ljung <ljung@live.se>
2016-12-05 22:17:50 +0100

    Solved part two of day 2

--- AdventOfCode/Day2/Program.cs
+++ AdventOfCode/Day2/Program.cs
@@ -9,21 +9,67 @@ namespace Day2
         static int[,] KeyPad = new int[3, 3] {  {1,2,3} ,
                                                 {4,5,6} ,
                                                 {7,8,9} };
-        static int Row = 1;
-        static int Column = 1;
+        static char[,] KeyPad2 = new char[5, 5] {  {'-','-','1','-','-'},
+                                                   {'-','2','3','4','-'},
+                                                   {'5','6','7','8','9'},
+                                                   {'-','A','B','C','-'},
+                                                   {'-','-','D','-','-'}
+        };
+        static int Row;
+        static int Column;
 
 
         static void Main(string[] args)
         {
             var instructions = puzzleInput.Replace(" ", string.Empty).Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
 
+            // Part one
+            Row = 1;
+            Column = 1;
             foreach (var instruction in instructions)
             {
                 HandleInstruction(instruction);
                 Console.Write(KeyPad[Row, Column]);
             }
             Console.Write("\n");
+
+            // Part two
+            Row = 2;
+            Column = 0;
+            foreach (var instruction in instructions)
+            {
+                HandleInstruction2(instruction);
+                Console.Write(KeyPad2[Row, Column]);
+            }
+            Console.Write("\n");
+
             Console.Read();
+        }
+
+        static void HandleInstruction2(string instruction)
+        {
+            foreach (char c in instruction)
+            {
+                switch (c)
+                {
+                    case 'U':
+                        if (Row > 0 && KeyPad2[Row - 1, Column] != '-')
+                            Row--;
+                        break;
+                    case 'D':
+                        if (Row < 4 && KeyPad2[Row + 1, Column] != '-')
+                            Row++;
+                        break;
+                    case 'L':
+                        if (Column > 0 && KeyPad2[Row, Column - 1] != '-')
+                            Column--;
+                        break;
+                    case 'R':
+                        if (Column < 4 && KeyPad2[Row, Column + 1] != '-')
+                            Column++;
+                        break;
+                }
+            }
         }
 
         static void HandleInstruction(string instruction)
